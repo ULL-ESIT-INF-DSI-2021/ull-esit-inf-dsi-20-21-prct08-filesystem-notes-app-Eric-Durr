42ccb7ee9e16287b7461e771e224c962504ce7f2
Eric DS <eric.d.s.info@gmail.com>
2021-04-24 20:44:34 +0100

    creating non default note

--- src/note.ts
+++ src/note.ts
@@ -1,13 +1,18 @@
 import {User} from "./user"
 
+type color = "default" | "white" | "red" | 
+             "black" | "pink" | "yellow" | 
+             "blue" | "green" | "orange"
 class Note {
   public readonly user: User | undefined;
   static empty_entities: number = 0;
   constructor(public title: string = "", 
               public body: string = "", 
-              public color:string = "default",
+              public color: color = "default",
               user?: User) {
-                this.user = user;
+                if (user) {
+                  this.user = user;
+                }
                 if (this.title === "") {
                   Note.empty_entities++;
                   this.title = "Empty note " + Note.empty_entities.toString();
--- tests/note.spec.ts
+++ tests/note.spec.ts
@@ -1,6 +1,7 @@
 import "mocha";
 import {expect} from "chai";
-import {Note} from "../src/note"
+import {User} from "../src/user";
+import {Note} from "../src/note";
 
 describe("Default Note class tests", ()=> {
   it("Note creation returns no user, empty content and default color", ()=>{
@@ -9,5 +10,26 @@ describe("Default Note class tests", ()=> {
     expect(defaultNote.title).to.be.eq("Empty note 1");
     expect(defaultNote.body).to.be.eq("");
     expect(defaultNote.color).to.be.eq("default");
+  })
+  it("Second note increases the empty note counter", ()=>{
+    let defaultNote: Note = new Note();
+    expect(defaultNote.user).to.be.eq(undefined);
+    expect(defaultNote.title).to.be.eq("Empty note 2");
+    expect(defaultNote.body).to.be.eq("");
+    expect(defaultNote.color).to.be.eq("default");
+  })
+})
+
+describe("Note class tests", ()=> {
+  it("Note creation returns user, content if inputed andcolor", ()=>{
+    let ericUser: User = new User("Eric");
+    let newNote: Note = new Note("Nueva nota", " some new content ", "red", ericUser);
+    expect(newNote.user).to.be.eq(ericUser);
+    if (newNote.user) {
+      expect(newNote.user.name).to.be.eq("Eric");
+    }  
+    expect(newNote.title).to.be.eq("Nueva nota");
+    expect(newNote.body).to.be.eq(" some new content ");
+    expect(newNote.color).to.be.eq("red");
   })
 })
