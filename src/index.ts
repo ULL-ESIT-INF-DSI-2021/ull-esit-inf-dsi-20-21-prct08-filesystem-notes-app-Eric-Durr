613a1095ecbc12cdb5c1774ef0f10414780f3cb4
Eric DS <eric.d.s.info@gmail.com>
2021-04-25 17:46:29 +0100

    parsing cli

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,112 @@
 // import {NoteList} from "./note_list"
 // import {User} from "./note_list"
 // import {Note} from "./note"
-console.log("p08");
 
-export function trueFunction(): boolean {
-  return true;
-}
+import  * as yargs from "yargs";
+
+yargs
+.command('add', "Add a new note",{
+  user: {
+    describe: 'List owner',
+    demand: true,
+    alias: 'u',
+    type: "string"
+  },
+  title: {
+      describe: 'Note title',
+      demand: true,
+      alias: 't',
+      type: "string"
+  },
+  body: {
+      describe: 'Note body',
+      demand: false,
+      alias: 'b',
+      type: "string"
+  },
+  color: {
+      describe: 'Note color',
+      demand: false,
+      alias: 'c',
+      type: "string"
+  }
+})
+.command('modify', "Change text to existing note",{
+  user: {
+    describe: 'List owner',
+    demand: true,
+    alias: 'u',
+    type: "string"
+  },
+  title: {
+      describe: 'Note title',
+      demand: true,
+      alias: 't',
+      type: "string"
+  },
+  body: {
+      describe: 'Note body',
+      demand: false,
+      alias: 'b',
+      type: "string"
+  }
+})
+.command('append', "Add text to existing note",{
+  user: {
+    describe: 'List owner',
+    demand: true,
+    alias: 'u',
+    type: "string"
+  },
+  title: {
+      describe: 'Note title',
+      demand: true,
+      alias: 't',
+      type: "string"
+  },
+  body: {
+      describe: 'Note body',
+      demand: false,
+      alias: 'b',
+      type: "string"
+  }
+})
+.command('list', "List all notes", {
+  user: {
+    describe: 'List owner',
+    demand: true,
+    alias: 'u',
+    type: "string"
+  },
+})
+.command('remove', "Remove an existing note", {
+  user: {
+    describe: 'List owner',
+    demand: true,
+    alias: 'u',
+    type: "string"
+  },
+  title: {
+      describe: 'Note title',
+      demand: true,
+      alias: 't',
+      type: "string"
+  }
+})
+.command('read', "open a single note", {
+  user: {
+    describe: 'List owner',
+    demand: true,
+    alias: 'u',
+    type: "string"
+  },
+  title: {
+      describe: 'Title of TODO',
+      demand: true,
+      alias: 't'
+  }
+})
+.help('h')
+.argv;
+
+yargs.parse();
